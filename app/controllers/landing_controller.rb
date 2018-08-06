caaba9c72febec30d97a5d861793e51a948a4b68
kinjalt <kinjalthakkar.tps@gmail.com>
2018-08-06 17:06:25 +0530

    integrate contact us form

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,4 +1,31 @@
 class LandingController < ApplicationController
   def index
   end
+
+  def create
+    require 'net/http'
+    require 'json'
+    response = call_api
+    if response.kind_of? Net::HTTPSuccess
+      redirect_to root_path
+    else
+      json_response = JSON.parse(response.body)
+      json_response["errors"].each do |error|
+        @errors << error["field"] + " " + error["detail"]
+      end
+      render 'index'
+    end
+  end
+
+  private
+
+  def call_api
+    url = URI.parse(ENV['API_BASE_URL']+'/contact_messages')
+    http = Net::HTTP.new(url.host, url.port)
+    @errors = []
+    request = Net::HTTP::Post.new(url, {'Content-Type' => 'application/json'})
+    request.body = params.to_json
+    response = http.request(request)
+    return response
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,11 @@ module SsStudioLanding
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:put, :patch, :delete, :get, :post, :options]
+      end
+    end
   end
 end
